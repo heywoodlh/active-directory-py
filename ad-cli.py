6f8501c0173c7be6a41aa6b2e8f7a1cc593423fb
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-01-08 15:55:17 -0700

    working ldap login

--- ad-cli.py
+++ ad-cli.py
@@ -25,26 +25,6 @@ def init():
     username = domain + '\\' + username
 
     conn = ldap3.Connection(server, user="%s" % username, password='%s' % password, authentication=ldap3.NTLM, auto_bind=True) 
-    conn.extend.standard.who_am_i()
-
-#    bases = domain.split('.')
-#    bases_total = len(bases) 
-#    global base_domain
-
-#    for base in bases:
-#        if bases.index(base) == '0':
-#            print(0)
-#            base_domain = 'dc=' + str(base) 
-#        elif bases.index(base) == bases_total - 1:
-#            print(3)
-#            base_domain+='dc=' + str(base)
-#        else:
-#            print(2)
-#            base_domain+=',dc=' + str(base) + ','
-
-#   print(base_domain)
-
-#def search():
 
 
 def main():
